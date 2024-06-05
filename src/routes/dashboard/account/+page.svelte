6d415305d8dff8993163fc4d3ec09913b52e2db1
Hunter Austin <hjadar15@protonmail.com>
2024-06-05 00:47:09 -0400

    check if premium with active_tier

--- src/routes/dashboard/account/+page.svelte
+++ src/routes/dashboard/account/+page.svelte
@@ -41,7 +41,7 @@
     <div class="alert-message flex flex-col content-between space-y-6">
       <h3 class="h3">Subscription</h3>
       {#if !isUsingMobileStore}
-        {#if userRecord?.terabyte_active}
+        {#if userRecord?.active_tier.length > 0}
           You have an active subscription through Stripe. You can manage your
           subscription there.
           <button
