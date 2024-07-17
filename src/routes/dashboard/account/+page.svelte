b79eea85f04dc46c9c0ea21b3a95d51b863674d6
Hunter Austin <hjadar15@protonmail.com>
2024-07-17 02:00:24 -0400

    add account deletion button

--- src/routes/dashboard/account/+page.svelte
+++ src/routes/dashboard/account/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import PocketBase from "pocketbase";
+
   export let data;
   $: ({ userRecord, token } = data);
 
@@ -23,6 +25,29 @@
         console.log(data);
         window.location.href = data.url;
       });
+  }
+
+  let deleteButtonText = "Delete Account";
+  let deleteConfirmationStep = 0;
+
+  async function deleteAccount() {
+    try {
+      const pb = new PocketBase("https://pb.blazedcloud.com");
+      await pb.collection("users").delete(userRecord?.id);
+      window.location.href = "/auth";
+    } catch (error) {
+      console.error("Error deleting account:", error);
+      alert("Failed to delete account. Please try again later.");
+    }
+  }
+
+  function handleDeleteClick() {
+    if (deleteConfirmationStep === 0) {
+      deleteButtonText = "Confirm?";
+      deleteConfirmationStep = 1;
+    } else {
+      deleteAccount();
+    }
   }
 </script>
 
@@ -52,6 +77,39 @@
             on:click={() => checkout()}
           >
             Subscribe with Stripe
+          </button>
+        {/if}
+      {:else}
+        You have an active subscription using either the Apple App Store or
+        Google Play Store. You can manage your subscription there.
+      {/if}
+    </div>
+  </aside>
+
+  <aside class="alert variant-filled-surface w-full">
+    <div class="alert-message flex flex-col w-full content-between space-y-6">
+      <h3 class="h3">Delete Account</h3>
+      {#if !isUsingMobileStore}
+        {#if userRecord?.active_tier.length > 0}
+          You have an active subscription through Stripe. You can manage your
+          subscription there.
+          <button
+            class="btn btn-primary bg-primary-700"
+            on:click={() =>
+              (window.location.href =
+                "https://billing.stripe.com/p/login/fZeaI4a7Teoycuc5kk")}
+          >
+            <a href="https://billing.stripe.com/p/login/fZeaI4a7Teoycuc5kk"
+              >Manage On Stripe</a
+            >
+          </button>
+        {:else if userRecord}
+          Deletes your account and all of your data. This action cannot be
+          <button
+            class="btn btn-primary bg-red-900 w-full"
+            on:click={() => handleDeleteClick()}
+          >
+            {deleteButtonText}
           </button>
         {/if}
       {:else}
